018c8b75450b50625c6739f66e490270bcca43ea
Kieran Elmes <kieran.elmes@postgrad.otago.ac.nz>
2022-05-04 16:16:43 +1200

    fix re-running net for summary stats

--- net_test_summary.py
+++ net_test_summary.py
@@ -20,27 +20,31 @@ plink_base = os.environ['PLINK_FILE']
 home_dir = os.environ.get("HOME")
 os.chdir(home_dir + "/work/gout-transformer")
 
-batch_size = 30
-num_epochs = 50
-train, test, geno, pheno, enc_ver = get_data()
+batch_size = 10
+# num_epochs = 50
+# train, test, geno, pheno, enc_ver = get_data()
+# net_file = "genotyped_p1e-1_encv-3_batch-180_epochs-1_p-65803_n-18776_net.pickle"
+net_file = "genotyped_p1e-1_encv-2_batch-180_epochs-100_p-65803_n-18776_net.pickle"
+with open(net_file + "_test.pickle", "rb") as f:
+    test = pickle.load(f)
+with open("genotyped_p1e-1_encv-2_geno_cache.pickle", "rb") as f:
+    geno = pickle.load(f)
+
 # net_name = "{}_ai-{}_batch-{}_epochs-{}_p-{}_n-{}_controlx-{}_net.pickle".format(
 #     plink_base, str(use_ai_encoding), batch_size, num_epochs, geno.tok_mat.shape[1], geno.tok_mat.shape[0], control_scale
 # )
-net_file = "ai-True_batch-180_epochs-150_p-65803_n-18776_net.pickle"
+# net_file = "ai-True_batch-180_epochs-150_p-65803_n-18776_net.pickle"
+# net_file = "new_enc_50_epochs_0596_test_network.pickle"
 
 max_seq_pos = geno.positions.max()
-device = torch.device('cuda:7')
-use_device_ids = [7]
+device = torch.device('cuda:0')
+use_device_ids = [0]
 net = get_transformer(geno.tok_mat.shape[1], max_seq_pos, geno.num_toks, batch_size, device)
 net = nn.DataParallel(net, use_device_ids).to(use_device_ids[0])
 net.load_state_dict(torch.load(net_file))
-net.to(device)
+net = net.to(device)
 
 # check binary accuracy on test set:
-# test_gout_correct = 0
-# test_gout_incorrect = 0
-# test_control_correct = 0
-# test_control_incorrect = 0
 test_iter = data.DataLoader(test, (int)(batch_size), shuffle=False)
 actual_vals = []
 predicted_vals = []
@@ -57,6 +61,16 @@ with torch.no_grad():
 
 test_results = pd.DataFrame({"Score": nn_scores, "Predicted": predicted_vals, "Actual": actual_vals})
 test_results["Correct"] = test_results["Predicted"] == test_results["Actual"]
+
+print("fraction correct: {:.4f}".format(np.sum(test_results["Correct"])/len(test_results)))
+
+pred_gout = test_results[test_results["Predicted"]]
+print("total predicted gout cases: {}".format(len(pred_gout)))
+print("fraction of predicted gout cases correct: {:.4f}".format(np.sum(pred_gout["Correct"])/len(pred_gout)))
+
+non_gout = test_results[test_results["Predicted"] == False]
+print("total predicted gout cases: {}".format(len(non_gout)))
+print("fraction of predicted non-gout cases correct: {:.4f}".format(np.sum(non_gout["Correct"])/len(non_gout)))
 
 # distribution of scores
 plt.close()
