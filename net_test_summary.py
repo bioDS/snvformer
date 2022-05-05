4bc58e44a0fb5e2be4e7d9b8b13827e6c34bdc73
Kieran Elmes <kieran.elmes@postgrad.otago.ac.nz>
2022-05-05 17:21:35 +1200

    summary plot tweaks

--- net_test_summary.py
+++ net_test_summary.py
@@ -15,6 +15,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
 plink_base = os.environ['PLINK_FILE']
 
 home_dir = os.environ.get("HOME")
@@ -24,7 +25,10 @@ batch_size = 10
 # num_epochs = 50
 # train, test, geno, pheno, enc_ver = get_data()
 # net_file = "genotyped_p1e-1_encv-3_batch-180_epochs-1_p-65803_n-18776_net.pickle"
-net_file = "genotyped_p1e-1_encv-2_batch-180_epochs-100_p-65803_n-18776_net.pickle"
+# net_file = "genotyped_p1e-1_encv-2_batch-180_epochs-100_p-65803_n-18776_net.pickle"
+# net_file = "genotyped_p1e-1_encv-3_batch-180_epochs-100_p-65803_n-18776_net.pickle"
+# net_file = "genotyped_p1e-1_encv-2_batch-180_epochs-150_p-65803_n-18776_net.pickle"
+net_file = "66k_gwas_encv-2_batch-180_epochs-150_p-65803_n-18776_net.pickle"
 with open(net_file + "_test.pickle", "rb") as f:
     test = pickle.load(f)
 with open("genotyped_p1e-1_encv-2_geno_cache.pickle", "rb") as f:
@@ -37,8 +41,8 @@ with open("genotyped_p1e-1_encv-2_geno_cache.pickle", "rb") as f:
 # net_file = "new_enc_50_epochs_0596_test_network.pickle"
 
 max_seq_pos = geno.positions.max()
-device = torch.device('cuda:0')
-use_device_ids = [0]
+use_device_ids = [1]
+device = torch.device('cuda:{}'.format(use_device_ids[0]))
 net = get_transformer(geno.tok_mat.shape[1], max_seq_pos, geno.num_toks, batch_size, device)
 net = nn.DataParallel(net, use_device_ids).to(use_device_ids[0])
 net.load_state_dict(torch.load(net_file))
@@ -72,26 +76,50 @@ non_gout = test_results[test_results["Predicted"] == False]
 print("total predicted gout cases: {}".format(len(non_gout)))
 print("fraction of predicted non-gout cases correct: {:.4f}".format(np.sum(non_gout["Correct"])/len(non_gout)))
 
+sns.set_theme(style="whitegrid")
 # distribution of scores
 plt.close()
+fig, ax = plt.subplots(figsize=(3,2))
 fig = sns.histplot(
-    data=test_results, x="Score", bins=30
+    data=test_results, x="Score", bins=30, kde=True
 )
+# ax.set_xticks([0.0, 0.3, 0.5, 0.8])
+ax.set_xticks([0.1 * i for i in range(11)])
+ax.set_xticklabels([i for i in range(11)])
+ax.set_xlabel("Score $\\times 10^{-1}$")
 
-plt.savefig("score_dist.svg")
-plt.savefig("score_dist.png")
+plt.tight_layout()
+plt.savefig(net_file + "score_dist.pdf")
+plt.savefig(net_file + "score_dist.png")
 
 # score vs. accuracy
 # min_score = np.min(nn_scores)
 # max_score = np.max(nn_scores)
 # bins = np.arange(min_score, max_score, (max_score-min_score)/10)
 # test_results.sort_values
-test_results["Score (range)"] = pd.qcut(test_results["Score"], 10)
+# test_results["Score (range)"], bins = pd.qcut(test_results["Score"], 10, precision=1, retbins=True)
+test_results["Score (range)"], bins = pd.cut(test_results["Score"], [0.1 * i for i in range(11)], precision=1, retbins=True)
+# bin_titles = ["{:.0f}".format(10*i) for i in bins]
+bin_titles = ["{:.0f}-{:.0f}".format(10*i, 10*i + 1) for i in bins]
+test_results["Correct (%)"] = 100*test_results["Correct"]
 
 plt.close()
+fig, ax = plt.subplots(figsize=(4.5,2))
 fig = sns.pointplot(
-    data=test_results, x="Score (range)", y="Correct"
+    data=test_results, x="Score (range)", y="Correct (%)", ci=95, n_boot=1000,
+    markers='.'
 )
+for i, line in enumerate(ax.get_lines()):
+    if (i > 0):
+        line.set_alpha(0.5)
+# fig = sns.barplot(
+#     data=test_results, x="Score (range)", y="Correct", color="b"
+# )
+# ax.set_ylim((0.4, 0.8))
+ax.set_xticklabels(bin_titles[:-1])
+ax.set_xlabel("Score $\\times 10^{-1}$")
+# ax.set_yticks([0.5, 0.7])
 
-plt.savefig("score_vs_tp.svg")
-plt.savefig("score_vs_tp.png")
+plt.tight_layout()
+plt.savefig(net_file + "score_vs_tp.pdf")
+plt.savefig(net_file + "score_vs_tp.png")
