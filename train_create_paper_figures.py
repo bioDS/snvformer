d9dce35df0185cc83ce1e869aef8e1cf8f119911
Kieran Elmes <kelmes@cs.otago.ac.nz>
2022-06-20 16:52:50 +1200

    ternary run parameters

--- train_create_paper_figures.py
+++ train_create_paper_figures.py
@@ -54,6 +54,52 @@ def geno_only():
     summarise_net(net, test, parameters, net_file)
 
 
+def pheno_ternary():
+    parameters = snp_network.default_parameters
+    parameters['pretrain_base'] = 'all_unimputed_combined'
+    parameters['plink_base'] = 'genotyped_p1e-1'
+    parameters['continue_training'] = False
+    parameters['train_new_encoder'] = True
+    parameters['use_device_ids'] = [4]
+    parameters['encoding_version'] = 5
+    parameters['test_frac'] = 0.25
+    parameters['verify_frac'] = 0.05
+    parameters['batch_size'] = 5
+    parameters['num_epochs'] = 50
+    parameters['lr'] = 1e-7
+    parameters['pt_lr'] = 1e-7
+    parameters['encoder_size'] = 65803  # the size of gneotyped_p1e-1
+    parameters['pretrain_epochs'] = 20
+    parameters['num_phenos'] = 3
+    parameters['use_phenos'] = True
+    parameters['output_type'] = 'tok'
+    parameters['embed_dim'] = 64
+    parameters['num_heads'] = 4
+    parameters['num_layers'] = 4
+    parameters['linformer_k'] = 64
+    parameters['use_linformer'] = True
+
+    # TODO loading all this here is overkill
+    train_ids, train, test_ids, test, verify_ids, verify, geno, pheno, enc_ver = get_data(parameters)
+    pretrain_snv_toks = get_pretrain_dataset(train_ids, parameters)
+    #parameters['max_seq_pos'] = pretrain_snv_toks.positions.max()
+    #parameters['output_type'] = 'tok'
+    #parameters['num_toks'] = pretrain_snv_toks.num_toks
+
+    # reload trained network if it exists
+    net_file = saved_nets_dir + get_net_savename(parameters)
+    if os.path.exists(net_file):
+        print("reloading file: {}".format(net_file))
+        net = snp_network.get_transformer(parameters, pretrain_snv_toks)
+        net = nn.DataParallel(net, parameters['use_device_ids'])
+        net.load_state_dict(torch.load(net_file))
+        net = net.to(parameters['use_device_ids'][0])
+    else:
+        print("Training new net, no saved net in file '{}'".format(net_file))
+        net = snp_network.train_everything(parameters)
+
+    print("summarising test-set results")
+    summarise_net(net, test, parameters, net_file)
 def pheno_v1():
     parameters = snp_network.default_parameters
     parameters['pretrain_base'] = 'all_unimputed_combined'
@@ -106,5 +152,6 @@ if __name__ == "__main__":
     home_dir = os.environ.get("HOME")
     os.chdir(home_dir + "/work/gout-transformer")
 
-    geno_only()
+    # geno_only()
     # pheno_v1() # done
+    pheno_ternary()
